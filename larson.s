1f41534bef717c41f23f7b066ecc26c980065814
Blake Jones <blakemj@stanford.edu>
2018-10-05 23:46:29 -0700

    Created an extended Larson Scanner

--- larson.s
+++ larson.s
@@ -4,7 +4,11 @@
  *
  * This code will set up 8 LEDs to light up in order back and forth
  * like a Larson Scanner. This can also be used for 4 LEDs by changing
- * the NUMLED constant to 0x4.
+ * the NUMLED constant to 0x4. This scanner has three different brightness
+ * levels. The middle one follows the traditional pattern levels, and the
+ * LEDs to the left and right follow the middle LED at a lower brightness.
+ * The LEDs two spots away on each side also follow the middle LED at a very
+ * low brightness level.
  */
 
 .equ DELAY, 0x8000
@@ -21,12 +25,13 @@ orr r1, #0b1000000
 orr r1, #0b1001
 str r1, [r0] 
 
-// set bit 20
+// set bit 20, the two bits next to it, and the two bits two away
 mov r1, #(1<<20)
 mov r5, #(0b111<<19)
 mov r6, #(0b11111<<18)
 //set a counter for the number of LEDs
 mov r2, #NUMLED
+//set a timer for determining brightness levels
 mov r7, #20
 
 loop: 
@@ -35,35 +40,25 @@ loop:
 ldr r0, SET0
 str r1, [r0] 
 
-// delay
+// delay in a sequence to create 3 brightness levels
 mov r4, #DELAY
 wait1:
     sub r7, #1
     ldr r0, SET0
     str r1, [r0]
-    cmp r7, #3
+    cmp r7, #3 //loop until r7 brightness counter ends at value  (makes middle light very bright)
     bgt wait1
-    ldr r0, SET0
+    ldr r0, SET0 //no need to clear since middle LED still being lit up
     str r5, [r0]
-    cmp r7, #1
+    cmp r7, #1 //loop until r7 counter ends at value (makes next two lights medium brightness)
     bgt wait1
     ldr r0, SET0
     str r6, [r0]
-    ldr r0, CLR0
+    ldr r0, CLR0 //clear the registers to reset all the LEDs
     str r6, [r0]
-    mov r7, #20
-    subs r4, #1
+    mov r7, #20 //reset the brightness counter
+    subs r4, #1 //loop until the delay counter reaches 0
     bne wait1
-
-// set GPIO low
-//ldr r0, CLR0
-//str r1, [r0] 
-
-// delay
-//mov r4, #DELAY
-//wait2:
-//    subs r4, #1
-//    bne wait1
 
 //remove one from LED counter (How many LEDs left to turn on going FORWARD aka 20->27)
 //will branch to forward until needs to run in reverse
