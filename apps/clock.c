608534df66b9a7096e388a1dac721f72a4f4302e
Blake Jones <blakemj@stanford.edu>
2018-10-15 10:52:08 -0700

    Created an extension to show a digital clock that can be set for the current time with two buttons

--- apps/clock.c
+++ apps/clock.c
@@ -2,10 +2,12 @@
 #include "../timer.h"
 
 //These constants initialize and define the array of digits, and the first LEDs for the
-//digits and digit pieces in the sequence
+//digits and digit pieces in the sequence as well as the pins used for the buttons
 static char digits[16];
 static int FIRST_LED_GPIO = GPIO_PIN20;
 static int FIRST_DIGIT_GPIO = GPIO_PIN10;
+static int BUTTON_FOR_MINUTES = GPIO_PIN2;
+static int BUTTON_FOR_HOURS = GPIO_PIN3;
 
 /*
 * This function simply initializes the array of possible digit types 0-F. The LED parts
@@ -52,9 +54,9 @@ static void turn_on_light(int digit, char pattern) {
 
 /*
 * This function will simply set the digits from right to left to display the correct
-* pattern. The third digit is set to display the ones from the seconds, the second
-* is set to display the tens from the seconds, the first is set to display the ones
-* from the minutes, and the zero pin is set to display the tens from the minutes. 
+* pattern. The third digit is set to display the ones from the minutes, the second
+* is set to display the tens from the minutes, the first is set to display the ones
+* from the hours, and the zero pin is set to display the tens from the hours. 
 * Depending on what number of given time a digit is on, it will find the pattern from
 * the pattern array that corresponds and return it.
 */
@@ -65,7 +67,7 @@ static char figure_out_pattern(int digit, int min, int tenMin, int hr, int tenHr
     } else if (digit == 2) {
         pattern = digits[tenMin];
     } else if (digit == 1) {
-        pattern = digits[hr] | 0b10000000;
+        pattern = digits[hr] | 0b10000000; //Unsures that the dot point is always on
     } else {
         pattern = digits[tenHr];
     }
@@ -77,10 +79,11 @@ static char figure_out_pattern(int digit, int min, int tenMin, int hr, int tenHr
 * It then initializes the start time of the program (so that the program starts at zero).
 * Then, in an endless loop, it will switch between the four digits and display the pattern
 * for the time on that digit. Once a given time number has reached its max (such as 9 sec
-* in the ones place for seconds before the tens place for seconds needs to be changed), it
+* in the ones place for minutes before the tens place for minutes needs to be changed), it
 * will increase the next time digit by one and will reset back to zero. Further, after one
-* second, the start of the time for one second is reset to the current raspberry pi's timer
-* and the seconds ones digit is increased by one. This repeats every second exactly.
+* minute, the start of the time for one minute is reset to the current raspberry pi's timer
+* and the minutes ones digit is increased by one. This repeats every minute exactly. This 
+* also allows for two buttons to be pressed--one to set the minutes and one for the hours.
 */ 
 void main(void)
 {
@@ -94,10 +97,17 @@ void main(void)
     while (1) {
         for (int digit = 0; digit < 4; digit++) {
             gpio_set_output(FIRST_DIGIT_GPIO + digit);
+
+            //Increase the minute by one after one minute has elasped
             if (timer_get_ticks() - start >= 1000000 * 60) {
                 min++;
                 start = timer_get_ticks();
             }
+
+            //Used to roll over the minutes to the next digit after the final number 
+            //for the digit is used--allows for time to increase after 10 minutes into
+            //the tens digit, after 60 min into the hours digit, and for the hours digits
+            //to reach no further than 12 before they roll back over to 1
             if (min == 10) {
                 tenMin++;
                 min = 0;
@@ -114,12 +124,22 @@ void main(void)
                 tenHr =0;
                 hr = 1;
             }
+
+            //Find the pattern for the given time for the given digit
+            //Throughout the loop, this displays all four numbers for the time
             char pattern = figure_out_pattern(digit, min, tenMin, hr, tenHr);
             turn_on_light(digit, pattern);
-            gpio_set_input(GPIO_PIN2);
-            gpio_set_input(GPIO_PIN3);
-            int minButton = gpio_read(GPIO_PIN2);
-            int hrButton = gpio_read(GPIO_PIN3);
+
+            //Initialize the GPIO pins to read the inputs from the buttons
+            gpio_set_input(BUTTON_FOR_MINUTES);
+            gpio_set_input(BUTTON_FOR_HOURS);
+            int minButton = gpio_read(BUTTON_FOR_MINUTES);
+            int hrButton = gpio_read(BUTTON_FOR_HOURS);
+ 
+            //The buttons are wired to always give 3.3V (aka 1) to the pins until they
+            //are pressed when the pins become wired to ground (0V aka 0)
+            //Gives the user a fifth of a second to depress the button before increasing
+            //again
             if (minButton == 0 && timer_get_ticks() - buttonPress >= 200000) {
                 min++;
                 buttonPress = timer_get_ticks();
