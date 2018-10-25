ed2d8e4773ace2d4f5282d5e44daf834358f5e7a
Blake Jones <blakemj@stanford.edu>
2018-10-25 02:44:51 -0700

    Finished the string library.

--- strings.c
+++ strings.c
@@ -1,5 +1,8 @@
 #include "strings.h"
 
+/*
+* This function will set n amount of data in s pointer to the int c. This will return the s pointer.
+*/
 void *memset(void *s, int c, size_t n)
 {
     unsigned char* mem = (unsigned char*)s;
@@ -9,6 +12,10 @@ void *memset(void *s, int c, size_t n)
     return s;
 }
 
+/*
+* This function will take in a src memory pointer and will copy n amount of data over to 
+* the dst pointer. This will then return the dst pointer.
+*/
 void *memcpy(void *dst, const void *src, size_t n)
 {
     unsigned char* dstMem = (unsigned char*)dst;
@@ -19,6 +26,9 @@ void *memcpy(void *dst, const void *src, size_t n)
     return dst;
 }
 
+/*
+* This function will simply return the length of a string.
+*/
 int strlen(const char *s)
 {
     int i;
@@ -26,6 +36,10 @@ int strlen(const char *s)
     return i;
 }
 
+/*
+* This function will take in two strings and compare them. If they are equal, it will return 0. If s1 is larger
+* than s2 (based on ascii values), then it will return 1. If the opposite is true, then it will return -1.
+*/
 int strcmp(const char *s1, const char *s2)
 {
     int s1Length = strlen(s1);
@@ -37,6 +51,9 @@ int strcmp(const char *s1, const char *s2)
     return -1;
 }
 
+/*
+* This function will append the src string to the dst string up until a maxsize. The length of this string is then returned.
+*/
 int strlcat(char *dst, const char *src, int maxsize)
 {
     int dstSizeInitial = strlen(dst);
@@ -48,6 +65,9 @@ int strlcat(char *dst, const char *src, int maxsize)
     return strlen(dst);
 }
 
+/*
+* This funtion will multiply a number by its base a number of times until the digit holds its necessary value.
+*/
 static unsigned int numToBeMultiplied(int i, int base) {
     int num = 1;
     while (i != 0) {
@@ -57,6 +77,12 @@ static unsigned int numToBeMultiplied(int i, int base) {
     return num;
 }
 
+/*
+* This function will loop through all of the characters in the string and determine the characters value as a hex or decimal
+* number. A running total is kept, but each digit is added after being passed into the numToBeMultiplied function which will
+* multiply it by its base the number of times necessary given its place (for instance the 1 in 100 is larger than the 1 in 10).
+* This function returns that total.
+*/
 static unsigned int findingNum(const char *str, int lastNum, int base) {
     unsigned int total = 0;
     for (int a = 0; a <= lastNum; a++) {
@@ -69,20 +95,30 @@ static unsigned int findingNum(const char *str, int lastNum, int base) {
     return total;
 }
 
+/*
+* This function is called to take a string and convert it to a number. If the string has some non-number components
+* to it after the number, a pointer will point to this, and the number will be returned. This will be able to take in
+* a hex number of any case as well.
+*/
 unsigned int strtonum(const char *str, const char **endptr)
 {
     int lastNum = 0;
     volatile int base = 10;
     int initLength = strlen(str);
     for (int i = 0; i < initLength; i++) {
+
+        //This part is for when a number is a hex number
         if (str[0] == '0' && str[1] == 'x') {
             base = 16;
+            //This will check to see if the character is a hex number; if not, the endptr will point to it, and it will break from the loop
             if (i > 1 && (str[i] < '0' || (str[i] > '9' && str[i] < 'A') || (str[i] > 'F' && str[i] < 'a') || str[i] > 'f')) {
                 lastNum = i - 1;
                 *endptr = &str[i];
                 break;
             }
+        //This part of string to num is when a string is not a hex number
         } else {
+            //If the string is a decimal number, it will enter this and break from the loop once a non-decimal character is reached
             if (str[i] < '0' || str[i] > '9') {
                 lastNum = i - 1;
                 *endptr = &str[i];
